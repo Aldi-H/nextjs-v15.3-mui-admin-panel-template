368fa1bed36b6624af45338da5f15c10a256bec3
Aldi-H <kuro_19@Kuro19.>
2025-05-18 19:25:36 +0700

    refactor: enhance ESLint configuration with unicorn plugin rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
@@ -26,8 +27,80 @@ const eslintConfig = [
           case: "kebabCase",
         },
       ],
+      "unicorn/import-style": "off",
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          replacements: {
+            e: {
+              error: false,
+              event: false,
+            },
+            env: {
+              environment: false,
+            },
+            err: {
+              error: true,
+            },
+            ev: {
+              event: true,
+            },
+            evt: {
+              event: true,
+            },
+            idx: {
+              index: false,
+            },
+            msg: {
+              message: false,
+            },
+            props: {
+              properties: false,
+            },
+            prop: {
+              property: false,
+            },
+            param: {
+              parameter: false,
+            },
+            params: {
+              parameters: false,
+            },
+            res: {
+              resource: true,
+              response: true,
+              result: true,
+            },
+            src: {
+              source: true,
+            },
+            temp: {
+              temporary: true,
+            },
+            tmp: {
+              temporary: true,
+            },
+            util: {
+              utility: true,
+            },
+            utils: {
+              utilities: true,
+            },
+            val: {
+              value: true,
+            },
+            var: {
+              variable: true,
+            },
+            vars: {
+              variables: true,
+            },
+          },
+        },
+      ],
     },
   }),
+  eslintPluginUnicorn.configs.recommended,
 ];
 
 export default eslintConfig;
